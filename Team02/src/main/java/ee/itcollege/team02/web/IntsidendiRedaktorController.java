28d0c1bc636d54b4632e236fe171d54d683b2ca5
Ilmar Telga <ilmar255@hot.ee>
2011-11-28 15:51:48 +0200

    Intsidendi redaktor v0.1

--- Team02/src/main/java/ee/itcollege/team02/web/IntsidendiRedaktorController.java
+++ Team02/src/main/java/ee/itcollege/team02/web/IntsidendiRedaktorController.java
@@ -3,10 +3,14 @@ package ee.itcollege.team02.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ee.itcollege.team02.entities.INTSIDENT;
 
 @RequestMapping("/intsidendiredaktor/**")
 @Controller
@@ -17,11 +21,22 @@ public class IntsidendiRedaktorController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
-    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) 
+    {
+        
+        
+    }
+    
+    @RequestMapping(params = "id", method = RequestMethod.GET)
+    public void getIntsident(@RequestParam("id") Long id, Model uiModel)
+    {
+    	INTSIDENT intsident = INTSIDENT.findINTSIDENT(id);
+        uiModel.addAttribute("intsident", intsident);
     }
 
     @RequestMapping
-    public String index() {
+    public String index() 
+    {
         return "intsidendiredaktor/index";
     }
 }
